4dc6405a229b4bb47c2b95d28766bd4e503d5bad
InubashiriLix <lixinrong4002@gmail.com>
2025-07-17 06:19:54 +0800

    add the datetime str to datetime convert function

---
+++ src/utils.py
@@ -0,0 +1,29 @@
+import re
+from datetime import datetime
+
+
+def verify_email_format(email: str) -> bool:
+    regex = r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b"
+    if re.fullmatch(regex, email):
+        return True
+    else:
+        return False
+
+
+def str_to_datetime(datetime_str: str) -> datetime:
+    """
+    Convert a string to a datetime object.
+
+    Args:
+        datetime_str (str): The string representation of the datetime.
+
+    Returns:
+        datetime: The corresponding datetime object.
+    Raises:
+        ValueError: If the string is not in the correct format.
+    """
+    return datetime.strptime(datetime_str, "%Y-%m-%d %H:%M:%S")
+
+
+if __name__ == "__main__":
+    print(verify_email_format("s@gmail.comfads@gmail.com"))
