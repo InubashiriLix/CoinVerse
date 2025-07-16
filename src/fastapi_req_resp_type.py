fc9d8b65b7384ca71b9bc26ecea8a503721606f5
InubashiriLix <lixinrong4002@gmail.com>
2025-07-17 06:20:03 +0800

    update many

--- src/fastapi_req_resp_type.py
+++ src/fastapi_req_resp_type.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, List, Dict, Tuple
 from pydantic import BaseModel, Field
 
 
@@ -72,3 +72,153 @@ class ChangePasswordRequest(BaseModel):
 class ChangePasswordResponse(BaseModel):
     success: bool
     msg: str
+
+
+class CreateAccountBookRequest(BaseModel):
+    token: str = Field(...)
+    book_name: str = Field(...)
+
+
+class CreateAccountBookResponse(BaseModel):
+    """
+    Attributes:
+        success: the ops status
+        code:
+            # 0: success
+            # 1: token lost
+            # 2: token expired
+            # 3. book name duplicated
+        msg: information about the operation
+    """
+
+    success: bool = Field(...)
+    code: int = Field(...)
+    msg: str = Field(...)
+
+
+class ListBookRequest(BaseModel):
+    token: str = Field(...)
+
+
+class ListBookResponse(BaseModel):
+    """
+    Attributes:
+        success: the ops status
+        code:
+            # 0 success
+            # 1 token lost
+            # 2 token expired
+        msg: info ops
+        books: list of books
+    """
+
+    success: bool = Field(...)
+    code: int = Field(...)
+    msg: str = Field(...)
+    books: List[Dict[int, Tuple[str, float]]]
+
+
+class RemoveBookRequest(BaseModel):
+    token: str = Field(...)
+    book_id: int = Field(...)
+
+
+class RemoveBookResponse(BaseModel):
+    """
+    Attributes:
+        success: the ops status
+        code:
+            # 0 success
+            # 1 token lost
+            # 2 token expired
+        msg: info ops
+    """
+
+    success: bool = Field(...)
+    code: int = Field(...)
+    msg: str = Field(...)
+
+
+class BookDetailRequest(BaseModel):
+    token: str = Field(...)
+    account_book_id: int
+    start_time: str = Field(...)
+    end_time: str = Field(...)
+    note: str = Field(...)
+
+
+class BookDetailResponse(BaseModel):
+    """
+    Attributes:
+        success: status
+        code:
+            # 0 success
+            # 1 invlaid start_time or end time
+            # 2 token lost
+            # 3 token expired
+            # 4 unkown
+        transactions: [TODO:attribute]
+    """
+
+    success: bool = Field(...)
+    msg: str = Field(...)
+    code: int = Field(...)
+    transactions: List[Dict[int, Tuple[str, str, float]]]
+
+
+class AddIncomeRequest(BaseModel):
+    token: str = Field(...)
+    account_book_id: int = Field(...)
+    amount: float = Field(...)
+    time: str = Field(...)
+    note: str = Field(...)
+    income_idx: int = Field(...)
+
+
+class AddIncomeResponse(BaseModel):
+    """
+    Attributes:
+        success: [TODO:attribute]
+        msg: [TODO:attribute]
+        code:
+            # 0 success
+            # 1 invalid time str format
+            # 2 requireInfo lost (useless
+            # 3 token lost
+            # 4 token expired
+            # 5 unkown
+    """
+
+    success: bool = Field(...)
+    msg: str = Field(...)
+    code: int = Field(...)
+
+
+class AddOutcomeRequest(BaseModel):
+    token: str = Field(...)
+    account_book_id: int = Field(...)
+    amount: float = Field(...)
+    time: str = Field(...)
+    note: str = Field(...)
+    outcome_idx: int = Field(...)
+
+
+class AddOutcomeResponse(BaseModel):
+    """
+    Attributes:
+        success: [TODO:attribute]
+        msg: [TODO:attribute]
+        code:
+            # 0 success
+            # 1 invalid time str format
+            # 2 token lost
+            # 3 token expired
+            # 4 unkown
+    """
+
+    success: bool = Field(...)
+    msg: str = Field(...)
+    code: int = Field(...)
+
+
+# NOTE: may be I should have a ops praraphase to the app so that it can have a more collective operation
